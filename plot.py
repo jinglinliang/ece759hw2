987e246f865b5cd9b2165b559851a62a6aa1a387
jinglinliang <jinglin@cs.wisc.edu>
2019-02-07 02:10:23 -0600

    p1 plot

--- plot.py
+++ plot.py
@@ -1,10 +1,10 @@
 import matplotlib.pyplot as plt
 
 x = [10,11,12,13,14,15,16,17,18,19]
-y1 = [1,4,9,16,23,24,25,26,27,28]
-y2 = [1,5,9,18,23,27,28,29,30,31]
-plt.plot(x, y1, 'r-', x, y2, 'b-')
-plt.axis([9, 20, 0, 32])
+y1 = [3,13,47,149,473,1547,5505,21384,84908,338697]
+y2 = [0,0,1,2,5,11,21,41,77,142]
+plt.plot(x, y1, 'b-', x, y2, 'r-')
+plt.axis([9, 20, 0, 5000])
 plt.ylabel('Time in ms')
 plt.xlabel('Order of input size')
 plt.show()
